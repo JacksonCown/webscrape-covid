0d82ab84ae263120dcf508e1f8a8817ccbda3750
Jackson Cown <83671586+JacksonCown@users.noreply.github.com>
2022-08-01 21:46:11 -0400

    Create Basic WebApp

--- app.py
+++ app.py
@@ -1,3 +1,49 @@
-import dash
+from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
-app = dash.Dash(__name__)
+import pandas as pd
+
+app = Dash(__name__)
+print("Starting")
+
+app.layout = html.Div(children=[
+    html.H1(children='Database Management - CSCI 4370'),
+
+    html.Div(children='''
+        Select Response Variable:
+    '''),
+
+    dcc.RadioItems(
+        id='metric', 
+        #options=["Confirmed", "Deaths", "Testing_Rate", "Mortality_Rate"],
+        options=[
+            {'label': "Confirmed Cases", 'value': 'Confirmed'},
+            {'label': "Confirmed Deaths", 'value': 'Deaths'},
+            {'label': "Testing Rate", 'value': 'Testing_Rate'},
+            {'label': "Mortality Rate", 'value': 'Mortality_Rate'}
+        ],
+        value="Confirmed",
+        inline=True
+    ),
+
+    dcc.Graph(
+        id='graph'
+        #figure=fig
+    )
+])
+
+@app.callback(
+    Output("graph", "figure"), 
+    Input("metric", "value"))
+def display_choropleth(metric):
+    df = pd.read_csv("data/bigTable.csv")
+    # Our Graph
+    fig = px.choropleth(df, locations='Province_State', locationmode="USA-states", color=metric,
+                           animation_frame="Date",
+                           color_continuous_scale="Blackbody_r",
+                           scope="usa")
+
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    return fig
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
