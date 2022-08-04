88b75a07df0fb3cf0f5ba873cdd7f76db878d120
Jackson Cown <83671586+JacksonCown@users.noreply.github.com>
2022-08-03 22:35:04 -0400

    Final Changes for Submit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# webscrape-covid
- Scraping and cleaning covid data
+Database Management - CSCI 4370
+Term Project Submission
+
+Manager: Matthew Kurien
+Programmers: Jackson Cown, Arjun Kavungal
+
+Project: Creating a COVID-19 visualizations web-app based on the Dash Framework
+
+Components: 
+cleaning.py:
+- Scraping and cleaning data + munging/reformatting dataframes in pandas
+- Generating subtables for Database
+- Preparing data to prototype plots
+
+plotTest.py:
+- Prototyping interactive Plotly visualizations
+- Preparing Visualizations to transfer to Dash web-app
+
+app.py:
+- Main Dash web-app component
+- Executed to begin hosting the Dash app on local machine
+
+To Run Demo:
+Ensure you have the latest version of Dash and Pandas
+1. Navigate to the project directory
+2. Execute the command: "python app.py"
+3. Visit http://127.0.0.1:8050/ from your web-browser
+
+Note: There is a bug in the current implementation of the web-app that
+prevents the multi-select prompt from clearing its memory. If the page stops
+responding after you try to change the graph type, refresh and try again and
+it should fix it.
+
+Email me if you have any issues starting the demo:
+jhc47191@uga.edu.
+
+Thanks
--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app.layout = html.Div(id="page", children=[
     # Header
     html.H1(children='Database Management - CSCI 4370'),
 
+    html.Div(children="Choose Graph Data: "),
     # Dropdown Graph Select
     dcc.Dropdown(id='dropdown',
         options=[
@@ -17,7 +18,8 @@ app.layout = html.Div(id="page", children=[
             {'label': "Sex and Age USA", 'value': 'sexAge'},
             {'label': "Vaccination USA", 'value': 'vaccine'}
         ],
-        value="cases"),
+        value="cases",
+        persistence=False),
     html.Div(id="dropdown-output"),
 
     # Individual Graph Selects
@@ -32,7 +34,7 @@ app.layout = html.Div(id="page", children=[
     )
 ])
 
-# No clue how to break this into smaller subcomponents yet
+# Dynamically update graph components
 @app.callback(
     [Output("dropdown-output", "children"), Output("metric", "options"), Output("graph", "figure")], 
     [Input("dropdown", "value"), Input("metric", "value")])
@@ -46,10 +48,10 @@ def display_choropleth(dropdown, metric):
             {'label': "Mortality Rate", 'value': 'Mortality_Rate'}]
         
         # Read data and build DataFrame
-        df = pd.read_csv("data/bigTable.csv")
+        cases = pd.read_csv("data/bigTable.csv")
 
         # Render Graph
-        fig = px.choropleth(df, locations='Province_State', locationmode="USA-states", color=metric,
+        fig = px.choropleth(cases, locations='Province_State', locationmode="USA-states", color=metric,
                             animation_frame="Date",
                             color_continuous_scale="Blackbody_r",
                             scope="usa")
@@ -59,9 +61,17 @@ def display_choropleth(dropdown, metric):
 
         return f'Select Response Metric: ', choices, fig
     elif dropdown == "sexAge":
+        # Multichoice for Demographics Data Graph
+        choices = [{'label': "Male", 'value': 'male'},
+            {'label': "Female", 'value': 'female'},
+            {'label': "Both", 'value': 'both'}]
 
+        # Retrieve Data and build DataFrame
         sex_age = pd.read_csv("data/SexAndAge/COVID-19_Death_Counts_by_Age_2020-2022.csv")
+        # Compute Ratio of Total Deaths to Covid-19 Deaths for graphing
         sex_age = sex_age.assign(percent_deaths_from_covid = (sex_age["COVID-19 Deaths"] / sex_age["Total deaths"])*100)
+
+        # Update Based on Sex Toggle
         if metric == "male":
             male = sex_age[sex_age.Sex == "Male"]
             fig = px.bar(male, x="Age Years", y="percent_deaths_from_covid")
@@ -71,17 +81,22 @@ def display_choropleth(dropdown, metric):
         else:
             fig = px.bar(sex_age, x="Age Years", y="percent_deaths_from_covid", color="Sex")
 
-        choices = [{'label': "Male", 'value': 'male'},
-            {'label': "Female", 'value': 'female'},
-            {'label': "Both", 'value': 'both'}]
-        
         return f'Select Sex: ', choices, fig
     else:
+        # Multichoice for Vaccine Data Graph
+        choices = [{'label': "Moderna", 'value': 'Moderna'},
+                    {'label': "Janssen", 'value': 'Janssen'},
+                    {'label': "Pfizer", 'value': 'Pfizer'}, 
+                    {'label': "All", 'value': 'All'}]
+
+        # Access Data tables and build DataFrames
         vaccine_df = pd.read_csv("data/Vaccine2.csv")
         location = pd.read_csv("data/location.csv")
         
+        # Merge the DataFrames into one table
         vaccine_df = vaccine_df.merge(location, on='FIPS')
 
+        # Update Graph components based on Vaccine Type Toggle
         if metric == "Moderna":
             Moderna = vaccine_df[vaccine_df.Vaccine == "Moderna"]
             fig = px.choropleth(Moderna, locations="Province_State", locationmode="USA-states", color="Vax_Full",
@@ -113,10 +128,7 @@ def display_choropleth(dropdown, metric):
                             color_continuous_scale="RdBu",
                             scope="usa")
 
-        choices = [{'label': "Moderna", 'value': 'Moderna'},
-            {'label': "Janssen", 'value': 'Janssen'},
-            {'label': "Pfizer", 'value': 'Pfizer'}, 
-            {'label': "All", 'value': 'All'}]
+            fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
         
         return f'Select Vaccine Type: ', choices, fig
 
