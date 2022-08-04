e92115db28653bc05b1d2ca48ab3a4d2f015c681
Jackson Cown <83671586+JacksonCown@users.noreply.github.com>
2022-08-03 21:15:25 -0400

    Update Vaccine Table

--- app.py
+++ app.py
@@ -59,42 +59,66 @@ def display_choropleth(dropdown, metric):
 
         return f'Select Response Metric: ', choices, fig
     elif dropdown == "sexAge":
-        # Multichoice for Demographics data
-        choices = [{'label': "Male", 'value': 'male'},
-            {'label': "Female", 'value': 'female'},
-            {'label': "Both", 'value': 'both'}]
 
-        # Read data and build DataFrame
         sex_age = pd.read_csv("data/SexAndAge/COVID-19_Death_Counts_by_Age_2020-2022.csv")
-
-        # Swap data used for male, female, and both case
+        sex_age = sex_age.assign(percent_deaths_from_covid = (sex_age["COVID-19 Deaths"] / sex_age["Total deaths"])*100)
         if metric == "male":
             male = sex_age[sex_age.Sex == "Male"]
-            fig = px.bar(male, x="Age Years", y="COVID-19 Deaths")
+            fig = px.bar(male, x="Age Years", y="percent_deaths_from_covid")
         elif metric == "female":
             female = sex_age[sex_age.Sex == "Female"]
-            fig = px.bar(female, x="Age Years", y="COVID-19 Deaths")
+            fig = px.bar(female, x="Age Years", y="percent_deaths_from_covid")
         else:
-            fig = px.bar(sex_age, x="Age Years", y="COVID-19 Deaths", color="Sex")
+            fig = px.bar(sex_age, x="Age Years", y="percent_deaths_from_covid", color="Sex")
 
-        return f'Select Sex: ', choices, fig
-    else:
-        
-        # Multichoice for Demographics data
         choices = [{'label': "Male", 'value': 'male'},
             {'label': "Female", 'value': 'female'},
             {'label': "Both", 'value': 'both'}]
-        # Todo: Implement correct graph for vaccine table
-        # Place Holder Code Below
-        df = pd.read_csv("data/bigTable.csv")
-        # Our Graph
-        fig = px.choropleth(df, locations='Province_State', locationmode="USA-states", color=metric,
+        
+        return f'Select Sex: ', choices, fig
+    else:
+        vaccine_df = pd.read_csv("data/Vaccine2.csv")
+        location = pd.read_csv("data/location.csv")
+        
+        vaccine_df = vaccine_df.merge(location, on='FIPS')
+
+        if metric == "Moderna":
+            Moderna = vaccine_df[vaccine_df.Vaccine == "Moderna"]
+            fig = px.choropleth(Moderna, locations="Province_State", locationmode="USA-states", color="Vax_Full",
                             animation_frame="Date",
-                            color_continuous_scale="Blackbody",
+                            color_continuous_scale="RdBu",
                             scope="usa")
 
-        fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-        return f'Test', _,  fig
+            fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+        elif metric == "Janssen":
+            Janssen = vaccine_df[vaccine_df.Vaccine == "Janssen"]
+            fig = px.choropleth(Janssen, locations="Province_State", locationmode="USA-states", color="Vax_Full",
+                            animation_frame="Date",
+                            color_continuous_scale="RdBu",
+                            scope="usa")
+
+            fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+        elif metric == "Pfizer":
+            Pfizer = vaccine_df[vaccine_df.Vaccine == "Pfizer"]
+            fig = px.choropleth(Pfizer, locations="Province_State", locationmode="USA-states", color="Vax_Full",
+                            animation_frame="Date",
+                            color_continuous_scale="RdBu",
+                            scope="usa")
+
+            fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+        else:
+            all = vaccine_df[vaccine_df.Vaccine == "All"]
+            fig = px.choropleth(all, locations="Province_State", locationmode="USA-states", color="Vax_Full",
+                            animation_frame="Date",
+                            color_continuous_scale="RdBu",
+                            scope="usa")
+
+        choices = [{'label': "Moderna", 'value': 'Moderna'},
+            {'label': "Janssen", 'value': 'Janssen'},
+            {'label': "Pfizer", 'value': 'Pfizer'}, 
+            {'label': "All", 'value': 'All'}]
+        
+        return f'Select Vaccine Type: ', choices, fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
